57898ce0e13ca381f9cb4dfcde964ad662f46fc0
ashishdarekar <ashish.darekar@tum.de>
2021-03-11 10:31:04 +0100

    Moved all the functions from the adapter to the functionObject

--- KratosOpenfoamAdapterFunctionObject/KratosOpenfoamAdapterFunctionObject.C
+++ KratosOpenfoamAdapterFunctionObject/KratosOpenfoamAdapterFunctionObject.C
@@ -32,7 +32,7 @@ Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::KratosOpenfoamAdapte
     const word& name,
     const Time& runTime,
     const dictionary& dict
-): fvMeshFunctionObject(name, runTime, dict), CoSimulationAdapter_()
+): fvMeshFunctionObject(name, runTime, dict)//, CoSimulationAdapter_()
 {
     read(dict);
 }
@@ -47,7 +47,34 @@ Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::~KratosOpenfoamAdapt
 
 bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::read(const dictionary& dict)
 {
-    CoSimulationAdapter_.configure();
+    //Connect to CoSimIO
+    //CoSimulationAdapter_.configure();
+    //std::cout << "CoSimulation Adapter : Configuration" << std::endl;
+
+    std::cout << "CoSimulation Adapter's function object : read()" << std::endl;
+
+    //Importing data at the beginning from Kratos Cosimulation using CoSimIO
+    CoSimIO::Info settings;
+    settings.Set("my_name", "Openfoam_adapter");
+    settings.Set("connect_to", "Kratos_CoSimulation");
+    settings.Set("echo_level", 1);
+    settings.Set("version", "1.25");
+
+    connect_info = CoSimIO::Connect(settings);
+    COSIMIO_CHECK_EQUAL(connect_info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Connected);
+    connection_name = connect_info.Get<std::string>("connection_name");
+
+    std::vector<double> receive_data;
+    connect_info.Clear();
+    connect_info.Set("identifier", "vector_of_pi");
+    connect_info.Set("connection_name", connection_name);
+    connect_info = CoSimIO::ImportData(connect_info, receive_data);
+
+    for(auto& value : receive_data)
+    {
+        std::cout<< value << std::endl;
+        COSIMIO_CHECK_EQUAL(value, 3.14);
+    }
 
     return true;
 }
@@ -55,7 +82,7 @@ bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::read(const dict
 
 bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::execute()
 {
-    CoSimulationAdapter_.execute();
+    //CoSimulationAdapter_.execute();
 
     return true;
 }
@@ -63,7 +90,22 @@ bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::execute()
 
 bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::end()
 {
-    CoSimulationAdapter_.end();
+    //Dicsonect from CoSimIO
+    //CoSimulationAdapter_.end();
+
+    std::cout << "CoSimulation Adapter's function object : end()" << std::endl;
+
+    //Exporting the dummy vector data from OpenFOAM to the CoSimulation using CoSimIO
+    std::vector<double> data_to_send(4,3.14);
+    connect_info.Clear();
+    connect_info.Set("identifier", "vector_of_pi");
+    connect_info.Set("connection_name", connection_name);
+    connect_info = CoSimIO::ExportData(connect_info, data_to_send);
+
+    CoSimIO::Info disconnect_settings;
+    disconnect_settings.Set("connection_name", connection_name);
+    connect_info = CoSimIO::Disconnect(disconnect_settings); // disconnect afterwards
+    COSIMIO_CHECK_EQUAL(connect_info.Get<int>("connection_status"), CoSimIO::ConnectionStatus::Disconnected);
 
     return true;
 }
@@ -72,8 +114,7 @@ bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::end()
 bool Foam::functionObjects::KratosOpenfoamAdapterFunctionObject::write()
 {
     //adapter_.write();
-
-    cout<<"Welcome to the Kratos_OpenFOAM_Adapter\n";
+    //cout<<"Welcome to the Kratos_OpenFOAM_Adapter\n";
 
     return true;
 }
