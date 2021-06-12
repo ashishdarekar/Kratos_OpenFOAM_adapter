e2b608a75082a55cc09377d9e98247a6fe3259ee
ashishdarekar <ashish.darekar@tum.de>
2021-06-12 18:48:48 +0530

    Python script added to plot pressure and viscous forces in the simulation

---
+++ Tutorial_benchmark_with_CoSimIO/plot_forces_prep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gnuplot -persist > /dev/null 2>&1 << EOF
+	set title "Forces vs. Time"
+	set xlabel "Time / Iteration"
+	set ylabel "Force (N)"
+
+	plot	"forces.txt" using 1:2 title 'Pressure Forces' with linespoints,\
+			"forces.txt" using 1:3 title 'Viscous Forces' with linespoints
+EOF
