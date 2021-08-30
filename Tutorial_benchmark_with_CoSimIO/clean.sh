c67b072a71549d0b25529110ecdc0fdc16b0758b
ashishdarekar <ashish.darekar@tum.de>
2021-08-30 12:39:08 +0200

    modified clean script

--- Tutorial_benchmark_with_CoSimIO/clean.sh
+++ Tutorial_benchmark_with_CoSimIO/clean.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e -u
 
-echo "--- Cleaning OpenFOAM result files in $(pwd)"
-if [ -n "${WM_PROJECT:-}" ] || error "No OpenFOAM environment is active."; then
-    # shellcheck disable=SC1090 # This is an OpenFOAM file which we don't need to check
-    . "${WM_PROJECT_DIR}/bin/tools/CleanFunctions"
-    cleanCase
-    rm -rfv 0/uniform/functionObjects/functionObjectProperties
-fi
-
 echo "--- Cleaning CoSimulation result files in $(pwd)"
 rm -rfv .CoSimIOFileComm_Openfoam_Adapter_Openfoam_Kratos_Wrapper/
 rm -rfv vtk_output/
@@ -17,3 +9,11 @@ rm -rfv vtk_output_coupling/
 echo "--- Cleaning Post-processing files in $(pwd)"
 rm disp.txt
 rm forces.txt
+
+echo "--- Cleaning OpenFOAM result files in $(pwd)"
+if [ -n "${WM_PROJECT:-}" ] || error "No OpenFOAM environment is active."; then
+    # shellcheck disable=SC1090 # This is an OpenFOAM file which we don't need to check
+    . "${WM_PROJECT_DIR}/bin/tools/CleanFunctions"
+    cleanCase
+    rm -rfv 0/uniform/functionObjects/functionObjectProperties
+fi
