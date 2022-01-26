334e5da25ac0c79ba6e75b6b24bce8034ee70ac7
ashishdarekar <ashish.darekar@tum.de>
2022-01-26 21:01:53 +0100

    CAARC simulation files cleaning

--- Tutorial_benchmark_2_CAARC_with_MPI/plot_displacements.py
+++ Tutorial_benchmark_2_CAARC_with_MPI/plot_displacements.py
@@ -3,9 +3,12 @@
 import os
 import sys
 from math import *
+from time import time
 import matplotlib.pyplot as plt
+import random
+import numpy as np
 
-disp_file = "postProcessing/probes/0/cellDisplacement"
+disp_file = "output_csd/point_1.dat"
 
 if not os.path.isfile(disp_file):
 	print ("File not found at "+ disp_file)
@@ -24,7 +27,7 @@ while dis:
 		time_list_data = splited_data[0]
 		time_list.append(float(time_list_data))
 		#print(time_list)
-		displacement_data = splited_data[2]
+		displacement_data = splited_data[3]
 		if(displacement_data != "-1.79769313486e+307"):
 			displacement.append(float(displacement_data))
 			old_displacement_data = displacement_data
@@ -35,15 +38,19 @@ while dis:
 	dis = file.readline()
 
 #Plotting
-""" xmin, xmax = plt.xlim()
-ymin, ymax = plt.ylim() """
-
 plt.plot(time_list, displacement,color='b', linewidth=0.5)
+#plt.ylim(-1.053e-2,-1.060e-2) # point 1
+#plt.ylim(-1.78e-2,-1.66e-2) # point 2
+#plt.ylim(-2.16e-2,-2.06e-2) # point 3
+plt.xlim(0,25)
+
 plt.xlabel('Time')
 plt.ylabel('Displacement value')
-plt.title(" Displacement of interface flap's tip ")
-x = [0 , 0]
-y = [0, 50]
-plt.plot(y,x,'--', color = 'k')
+plt.title("Displacement of a point in CAARC")
+plt.suptitle("Kratos-OpenFOAM FSI simulation")
+#x = [0 , 0]
+#y = [3000, 3500]
+#plt.plot(y,x,'--', color = 'k')
 plt.show()
+
 
