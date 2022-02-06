58433ec6967f30b32e9bcc58b61ece013131d1d8
ashishdarekar <ashish.darekar@tum.de>
2022-02-06 12:45:01 +0100

    Cleaning the case files for CAARC Case

--- Tutorial_case_2_CAARC/plot_displacements.py
+++ Tutorial_case_2_CAARC/plot_displacements.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-disp_file = "output_csd/point_1.dat"
+disp_file = "output_csd/point_4.dat"
 
 if not os.path.isfile(disp_file):
 	print ("File not found at "+ disp_file)
@@ -23,34 +23,26 @@ while dis:
 	if splited_data[0] == '#':
 		pass
 	else:
-		#print(len(splited_data))
 		time_list_data = splited_data[0]
 		time_list.append(float(time_list_data))
-		#print(time_list)
-		displacement_data = splited_data[3]
+		#displacement_data = splited_data[1]
+		displacement_data = sqrt(pow(float(splited_data[1]),2) + pow(float(splited_data[2]),2) + pow(float(splited_data[3]),2))
 		if(displacement_data != "-1.79769313486e+307"):
 			displacement.append(float(displacement_data))
 			old_displacement_data = displacement_data
 		else:
 			displacement.append(float(old_displacement_data))
-		#print(displacement)
 
 	dis = file.readline()
 
 #Plotting
 plt.plot(time_list, displacement,color='b', linewidth=0.5)
-#plt.ylim(-1.053e-2,-1.060e-2) # point 1
-#plt.ylim(-1.78e-2,-1.66e-2) # point 2
-#plt.ylim(-2.16e-2,-2.06e-2) # point 3
-plt.xlim(0,25)
+#plt.xlim(0,25)
 
 plt.xlabel('Time')
 plt.ylabel('Displacement value')
 plt.title("Displacement of a point in CAARC")
 plt.suptitle("Kratos-OpenFOAM FSI simulation")
-#x = [0 , 0]
-#y = [3000, 3500]
-#plt.plot(y,x,'--', color = 'k')
 plt.show()
 
 
